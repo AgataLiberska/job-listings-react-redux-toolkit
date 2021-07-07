7719b83b83eecd8b25d08a31e973f3e53d96acdc
Agata Liberska <liberska@gmail.com>
2021-07-07 17:01:58 +0100

    creat jobs slice

---
+++ src/data/jobListings.js
@@ -0,0 +1,197 @@
+export const jobListings = [
+    {
+        "id": 1,
+        "company": "Photosnap",
+        "logo": require("./images/photosnap.svg").default,
+        "new": true,
+        "featured": true,
+        "position": "Senior Frontend Developer",
+        "role": "Frontend",
+        "level": "Senior",
+        "postedAt": "1d ago",
+        "contract": "Full Time",
+        "location": "USA Only",
+        "languages": [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        "tools": []
+    },
+    {
+        "id": 2,
+        "company": "Manage",
+        "logo": require("./images/manage.svg").default,
+        "new": true,
+        "featured": true,
+        "position": "Fullstack Developer",
+        "role": "Fullstack",
+        "level": "Midweight",
+        "postedAt": "1d ago",
+        "contract": "Part Time",
+        "location": "Remote",
+        "languages": [
+            "Python"
+        ],
+        "tools": [
+            "React"
+        ]
+    },
+    {
+        "id": 3,
+        "company": "Account",
+        "logo": require("./images/account.svg").default,
+        "new": true,
+        "featured": false,
+        "position": "Junior Frontend Developer",
+        "role": "Frontend",
+        "level": "Junior",
+        "postedAt": "2d ago",
+        "contract": "Part Time",
+        "location": "USA Only",
+        "languages": [
+            "JavaScript"
+        ],
+        "tools": [
+            "React",
+            "Sass"
+        ]
+    },
+    {
+        "id": 4,
+        "company": "MyHome",
+        "logo": require("./images/myhome.svg").default,
+        "new": false,
+        "featured": false,
+        "position": "Junior Frontend Developer",
+        "role": "Frontend",
+        "level": "Junior",
+        "postedAt": "5d ago",
+        "contract": "Contract",
+        "location": "USA Only",
+        "languages": [
+            "CSS",
+            "JavaScript"
+        ],
+        "tools": []
+    },
+    {
+        "id": 5,
+        "company": "Loop Studios",
+        "logo": require("./images/loop-studios.svg").default,
+        "new": false,
+        "featured": false,
+        "position": "Software Engineer",
+        "role": "Fullstack",
+        "level": "Midweight",
+        "postedAt": "1w ago",
+        "contract": "Full Time",
+        "location": "Worldwide",
+        "languages": [
+            "JavaScript"
+        ],
+        "tools": [
+            "Ruby",
+            "Sass"
+        ]
+    },
+    {
+        "id": 6,
+        "company": "FaceIt",
+        "logo": require("./images/faceit.svg").default,
+        "new": false,
+        "featured": false,
+        "position": "Junior Backend Developer",
+        "role": "Backend",
+        "level": "Junior",
+        "postedAt": "2w ago",
+        "contract": "Full Time",
+        "location": "UK Only",
+        "languages": [
+            "Ruby"
+        ],
+        "tools": [
+            "RoR"
+        ]
+    },
+    {
+        "id": 7,
+        "company": "Shortly",
+        "logo": require("./images/shortly.svg").default,
+        "new": false,
+        "featured": false,
+        "position": "Junior Developer",
+        "role": "Frontend",
+        "level": "Junior",
+        "postedAt": "2w ago",
+        "contract": "Full Time",
+        "location": "Worldwide",
+        "languages": [
+            "HTML",
+            "JavaScript"
+        ],
+        "tools": [
+            "Sass"
+        ]
+    },
+    {
+        "id": 8,
+        "company": "Insure",
+        "logo": require("./images/insure.svg").default,
+        "new": false,
+        "featured": false,
+        "position": "Junior Frontend Developer",
+        "role": "Frontend",
+        "level": "Junior",
+        "postedAt": "2w ago",
+        "contract": "Full Time",
+        "location": "USA Only",
+        "languages": [
+            "JavaScript"
+        ],
+        "tools": [
+            "Vue",
+            "Sass"
+        ]
+    },
+    {
+        "id": 9,
+        "company": "Eyecam Co.",
+        "logo": require("./images/eyecam-co.svg").default,
+        "new": false,
+        "featured": false,
+        "position": "Full Stack Engineer",
+        "role": "Fullstack",
+        "level": "Midweight",
+        "postedAt": "3w ago",
+        "contract": "Full Time",
+        "location": "Worldwide",
+        "languages": [
+            "JavaScript",
+            "Python"
+        ],
+        "tools": [
+            "Django"
+        ]
+    },
+    {
+        "id": 10,
+        "company": "The Air Filter Company",
+        "logo": require("./images/the-air-filter-company.svg").default,
+        "new": false,
+        "featured": false,
+        "position": "Front-end Dev",
+        "role": "Frontend",
+        "level": "Junior",
+        "postedAt": "1mo ago",
+        "contract": "Part Time",
+        "location": "Worldwide",
+        "languages": [
+            "JavaScript"
+        ],
+        "tools": [
+            "React",
+            "Sass"
+        ]
+    }
+]
---
+++ src/features/jobs/jobsSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { jobListings } from '../../data/jobListings';
+
+const initialState = jobListings;
+
+const jobsSlice = createSlice({
+    name: 'jobs',
+    initialState,
+    reducers: {}
+});
+
+export default jobsSlice.reducer;
